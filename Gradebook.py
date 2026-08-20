06afb902e58525c2a912a9f5ea07ce81e42fb4af
MYahya-ee <muyahya270@gmail.com>
2026-08-21 01:54:47 +0500

    Added view all/single , rankings and  pass/fail

--- Gradebook.py
+++ Gradebook.py
@@ -1,7 +1,10 @@
+#GRADEBOOK
+
 import json
 from Student import Students
 from subject import Subject
-subjects = ["Maths", "English", "Physics", "Chemistry", "Biology", "Urdu", ]
+from itertools import zip_longest
+
 class GradeBook:
     def __init__(self):
         try:
@@ -10,45 +13,147 @@ class GradeBook:
             self.students = json.loads(content) if content.strip() else []
         except FileNotFoundError:
             self.students = []
+
     def add_student(self, student_dict):
         self.students.append(student_dict)
         with open("students_data.json", "w") as f:
               json.dump(self.students, f, indent=2)
-        with open("students_data.json", "r") as f:
-            info = json.load(f)
-        for student in info:
-            print(f"{student['name']} has been added having roll number {student['roll_number']}")
+        
          
-
     def add_grades(self, roll, sub_name, grade):
         for student in self.students:
             if roll == student["roll_number"]:
-                student["grades"].append(Subject(sub_name, grade).to_dict())
-                
+                student["grades"].append(Subject(sub_name, grade).to_dict())      
         with open("students_data.json", "w") as f:
             json.dump(self.students, f, indent=2)
         
     def view_all(self):
-        pass
+        with open("students_data.json", "r") as f:
+            info = json.load(f)
+        print()
+        for i in info:    
+            if i['grades']:
+                for grade in i['grades']: 
+                    grades_formatted = " | ".join(f"{g['name']}: {g['grade']}" for g in i['grades'])
+            else:
+                grades_formatted = "no grades yet"
+            print(f"{i['roll_number']} {i['name']:<12} {grades_formatted}")
+
+    def rankings(self):
+        lis_perc = self.get_percentage()
+        ranked = sorted(lis_perc, key=lambda x: x['percent'], reverse=True)
+        print()
+        for i, student in enumerate(ranked):
+            print(f"{i+1}. {student['name']:<12} {student['percent']:.2f}%")
+
+    def pass_fail(self, rollno):
+        list_perc = self.get_percentage()
+        for student in list_perc:
+            if rollno == student['roll_number']:
+                if int(student['percent']) < 50:
+                    print("Failed")
+                else:
+                    print(f"Passed with {student['percent']}%")
+
+        inp = input("Do you wanna see who passed and who failed? (Y/N) ").capitalize()
+        if inp == "Y":
+            failed_students = []
+            passed_students = []
+            for student in list_perc:
+                if int(student['percent']) < 50:
+                    failed_students.append(student['name'])
+                else:
+                    passed_students.append(student['name'])
+            
+            print(f"{'Passed students':<30} {'Failed Students'}")
+            print("-"*50)
+            for f, p in zip_longest(passed_students, failed_students, fillvalue=""):
+                print(f"{f:<30} {p}")
+        else:
+            print("Going back to menue")
+    def get_percentage(self):
+            perc_list_students = []
+            with open("students_data.json", "r") as f:
+                students = json.load(f)
+            for student in students:
+                if not student['grades']:
+                    continue
+                total_score = 0
+                for grade in student['grades']:
+                    total_score += int(grade['grade'])
+                percent = (total_score/(len(student['grades'])))
+                fin = {'name' : student['name'], 'percent': percent, "roll_number":student['roll_number']}
+                perc_list_students.append(fin)
+            return perc_list_students
+
+    def view_single(self, roll_number):
+        with open("students_data.json", "r") as f:
+            info = json.load(f) 
+        for i in info:
+            if roll_number == i['roll_number']:
+                if i['grades']:
+                    for grade in i['grades']: 
+                        grades_formatted = " | ".join(f"{g['name']}: {g['grade']}" for g in i['grades'])
+                else:
+                    grades_formatted = "no grades yet"
+                print(f"""Name: {i['name']}
+
+    
+
+Roll number: {i['roll_number']}
+Grades: {grades_formatted}""")
 
 def main():
-    #adding students
-    name = input("Enter the name of student: ")
-    s1 = Students(name)
     gradebook = GradeBook()
-    gradebook.add_student(s1.to_dict())
-    #adding subject and marks
-    roll = input("Enter roll number to add grades: ")
-
     while True:
-            sub_name = input("Enter the name of subject: ")
-            grade = input("Enter grade: (out of 100)")
-            gradebook.add_grades(roll, sub_name, grade)
+        print("""\nWhat would you like to do:
+1.Add a student
+2.Add a subject + grade
+3.View all students
+4.View a single student
+5.View rankings
+6.Check pass/fail
+7.Update a grade
+8.Delete a student
+9.Export results sheet
+0.Exit
+""")
+        
+        inp = input()
+        match inp:
+            case "0":
+                exit()
 
-            inp = input("Do you want to add more subjects and grades?").strip().capitalize()
-            if inp == "Yes":
-                 continue
-            else:
-                 break
-    
+            case "1": #Add a student
+                name = input("Enter the name of student: ")
+                s1 = Students(name)
+                gradebook.add_student(s1.to_dict())
+                print(f"\"{s1.name}\" has been added having roll number: {s1.roll_number}")
+
+            case "2": #adding subject and marks
+                roll = input("Enter roll number to add grades: ")
+                while True:
+                        sub_name = input("Enter the name of subject: ")
+                        grade = input("Enter marks (out of 100): ")
+                        gradebook.add_grades(roll, sub_name, grade)
+                        inp = input("Do you want to add more subjects and grades? (Y/N) ").strip().capitalize()
+                        if inp == "Y":
+                            continue
+                        else:
+                            break
+
+            case "3": #view all students
+                gradebook.view_all()
+
+            case"4":
+                rn = input("Enter the roll number of student: ")
+                gradebook.view_single(rn)    
+
+            case "5":
+                gradebook.rankings()    
+
+            case "6":
+                rollnumber = input("Enter the roll number: ")
+                gradebook.pass_fail(rollnumber)
+                
 main()
--- Student.py
+++ Student.py
@@ -1,16 +1,17 @@
 import uuid
+import json
 class Students:
     
     def __init__(self, name):
         self.name = name
         self.roll_number = str(uuid.uuid1())[:4]
         self.grades = []
-
     def to_dict(self):             
-                return {
-                    "name" : self.name,
-                    "roll_number" : self.roll_number,
-                    "grades" : self.grades
-                    
-                }
+        return {
+            "name" : self.name,
+            "roll_number" : self.roll_number,
+            "grades" : self.grades
+            
+        }
 
+    
